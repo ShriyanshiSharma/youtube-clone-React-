09fa336085cc608364e194d2e3bc481fd6f7ecd9
Shriyanshi Sharma <shriyanshi.19b131153@abes.ac.in>
2023-02-16 00:02:42 +0530

    commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { useState,useEffect } from "react";
+import{Box,Stack,Typography} from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
-    return;
+    return(
+        <Stack sx={{flexDirection:{sx:'column',md:'row'}}}>
+            <Box 
+            sx={{height:{sx:'auto',md:'92vh'}, 
+            borderRight:'1px solid #3d3d3d' ,px:{sx:0,md:2}}}>
+                Sidebar
+                <Sidebar/>
+                <Typography className="copyright" 
+                variant="boby2" sx={{mt:1.5,color:'#fff'}}>
+                    Copyright 2022 JSM Media
+                </Typography>
+            </Box>
+            <Box p={2} sx ={{ overfolw:'auto',height:'90vh',flex:2}}>
+                <Typography variant="h4" fontWeight="bold" mb={2} sx={{
+                    color:'white'
+                }}>New 
+                    <span style={{color:'#F31503'}}
+                    > videos</span>
+                </Typography>
+                <Videos/>
+            </Box>
+        </Stack>
+    );
 }
 export default  Feed;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Videos = () => (
+    <></>
+)
+export default  Videos;
