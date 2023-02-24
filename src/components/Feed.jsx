e6e5fa97270d4c1c5cff671967a6ea237f507447
Shriyanshi Sharma <shriyanshi.19b131153@abes.ac.in>
2023-02-24 20:51:36 +0530

    content

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -19,6 +19,7 @@ const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
@@ -30,9 +31,7 @@ const Feed = () => {
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
-
         <Videos videos={videos} />
-        
       </Box>
     </Stack>
   );
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,6 +5,7 @@ const Loader = () =>  (
   <Box minHeight="95vh">
     <Stack direction='row' justifyContent='center' alignItems='center' height='80vh' >
       <CircularProgress />
+      
     </Stack>
   </Box>
 );
